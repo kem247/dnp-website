5657759544a2b4695dede2a70f82ba210f181d1d
Kemi Thomas <kemothom71@gmail.com>
2020-06-15 19:30:26 -0400

    first commit

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,11 +4,13 @@ import { Button } from "react-bootstrap"
 import LabelWrapper from "../components/LabelWrapper/index.js"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-
-const SecondPage = () => 
-<Layout>
-<LabelWrapper />
-</Layout>
-
+var StatsD = require("node-dogstatsd").StatsD
+var dogstatsd = new StatsD()
+dogstatsd.increment("page.views")
+const SecondPage = () => (
+  <Layout>
+    <LabelWrapper />
+  </Layout>
+)
 
 export default SecondPage
