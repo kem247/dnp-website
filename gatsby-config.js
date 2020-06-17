2ee793121988b252c167e0617abf68c1e2cfee3c
Kemi Thomas <kemothom71@gmail.com>
2020-06-16 20:26:25 -0400

    datadog config

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,27 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        typeName: `GitHub`,
+        fieldName: `github`,
+        url: `https://api.github.com/graphql`,
+        headers: {
+          Authorization: `Bearer your-github-token`,
+        },
+      },
+
+      tests: [
+        {
+          id: "<TEST_PUBLIC_ID>",
+        },
+        {
+          id: "<TEST_PUBLIC_ID>",
+        },
+      ],
+    },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
