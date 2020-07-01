2aff472c9d091889c729ca2e2b86ae0f40b1fd13
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-01 17:56:40 +0000

    Deploy kem247/dnp-website to github.com/kem247/dnp-website.git:gh-pages

---
+++ src/pages/page-2.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+import { Button } from "react-bootstrap"
+import LabelWrapper from "../components/LabelWrapper/index.js"
+import Layout from "../components/layout"
+
+const SecondPage = () => (
+  <Layout>
+    <LabelWrapper />
+  </Layout>
+)
+
+export default SecondPage
